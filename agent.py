0db396f5119661687ccecb79cd3926f9c7cc7784
Hector <hectorcreatives08@gmail.com>
2023-07-16 20:24:42 -0400

    third commit

--- agent.py
+++ agent.py
@@ -14,7 +14,9 @@ class Agent:
         self.epsilon = 0 #randomness
         self.gamm = 0 #discount rate
         self.memory = deque(maxlen=MAX_MEMORY)
-        # TODO: model, trainer
+        self.model = None # TODO
+        self.trainer = None # TODO
+
 
     def get_state(self, game):
         head = game.snake[0]
@@ -63,13 +65,21 @@ class Agent:
         return np.array(state, dtype=int)
 
     def remenber(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
 
-    def train_short_memory(self):
-        pass
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        for state, action, reward, next_state, done in mini_sample:
+            self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
         pass
